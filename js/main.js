aac2882055a3aa4b5443b17e77f741b54c197847
Az-ber <azber7049@gmail.com>
2021-06-17 18:44:22 +0500

    portfolio fix

--- js/main.js
+++ js/main.js
@@ -1,3 +1,41 @@
 $(function () {
-  
+  $('.slick').slick({
+
+  });
+
+  wow = new WOW(
+    {
+      boxClass: 'wow',      // default
+      animateClass: 'animate__animated', // default
+      offset: 0,          // default
+      mobile: true,       // default
+      live: true        // default
+    }
+  );
+  wow.init();
+
+  var typed = new Typed('.header__desc--change', {
+    strings: [
+      "Frontend Developer",
+      "Student",
+      "Freelancer"
+    ],
+    typeSpeed: 150,
+    loop: true,
+    backspeed: 150,
+    startDelay: 200,
+  });
+
+  $(".menu a, .header__logo").on("click", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
+
+  $('.header__btn').on('click', function () {
+    $('.header__top').toggleClass('header__top--active');
+    $('.header__btn').toggleClass('header__btn--active');
+  });
+
 });
